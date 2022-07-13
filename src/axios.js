bd14265374496a0a0cc03bc61c10b1078dc20cc1
Pranairaj <kaligithipranai@gmail.com>
2022-07-13 14:10:51 +0530

    axios update

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // base url to make requests to the movie database
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3/movie/550",
+  baseURL: "http://api.themoviedb.org/3",
 });
 
 export default instance;
